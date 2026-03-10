0acdc913154c6248fcd4f096b669224de7d9434c
Juma <itsjumah.tj@gmail.com>
2026-03-10 19:41:52 +0200

    Add Push Notifications and Blocked Users settings pages with visibility options

---
+++ web/app/settings/notifications/push/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import Header from "@/app/components/settings/Header";
+import ToggleSwitch from "@/app/components/ToggleSwitch";
+
+export default function Page() {
+  const visibilityOptions = [
+    { label: "Make my profile public", state: true },
+    { label: "Make my posts public", state: false },
+    { label: "Make my comments public", state: true },
+    { label: "Show my online status", state: true },
+  ];
+
+  return (
+    <div className="pt-30 px-6 bg-[#F7F7F7] h-dvh">
+      <Header title="Push Notifications" />
+      <div className="space-y-3">
+        {visibilityOptions.map((option, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between bg-white p-3 rounded-lg shadow-sm"
+          >
+            <p className="text-sm text-[#3D3D3D]">{option.label}</p>
+            <ToggleSwitch
+              state={option.state}
+              onChange={(newState) =>
+                console.log("Profile visibility:", newState)
+              }
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ web/app/settings/privacy/blocked-users/page.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Header from "@/app/components/settings/Header";
+import { Trash } from "iconsax-reactjs";
+import ActionButton from "@/app/components/ActionButton";
+
+const blockedUsers = [
+  {
+    id: 1,
+    username: "ceo",
+  },
+  {
+    id: 2,
+    username: "dan_druff",
+  },
+  {
+    id: 3,
+    username: "john_doe",
+  },
+];
+
+export default function Page() {
+  return (
+    <div className="pt-30 px-6 bg-[#F7F7F7] h-dvh relative">
+      <Header title="Blocked Users" />
+      <div className="space-y-3">
+        {blockedUsers.map((user) => (
+          <div
+            key={user.id}
+            className="flex items-center justify-between bg-white p-3 rounded-lg shadow-sm"
+          >
+            <p className="text-sm text-[#3D3D3D]">{user.username}</p>
+            <Trash size={18} color="#E00505" />
+          </div>
+        ))}
+      </div>
+      <ActionButton className="absolute bottom-6 right-6 left-6">
+        Unblock all
+      </ActionButton>
+    </div>
+  );
+}
