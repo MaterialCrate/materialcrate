5441850a2249f38fe21d0270f6e4d7a8944601b2
Juma <itsjumah.tj@gmail.com>
2026-04-07 20:10:08 +0200

    feat(database): add achievementId column to Notification table for tracking user achievements

--- server/prisma/migrations/20260407_add_user_achievements/migration.sql
+++ server/prisma/migrations/20260407_add_user_achievements/migration.sql
@@ -12,3 +12,5 @@ CREATE INDEX "UserAchievement_userId_idx" ON "UserAchievement"("userId");
 CREATE INDEX "UserAchievement_achievementId_idx" ON "UserAchievement"("achievementId");
 
 ALTER TABLE "UserAchievement" ADD CONSTRAINT "UserAchievement_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE "Notification" ADD COLUMN IF NOT EXISTS "achievementId" TEXT;
